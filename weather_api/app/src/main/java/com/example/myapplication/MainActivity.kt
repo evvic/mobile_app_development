0842dff18d584fefeb64aef74838553f9b6c997b
Eric Brown <ewic98@gmail.com>
2022-01-15 14:10:10 +0700

    text change listener for city

--- weather_api/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ weather_api/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.myapplication
 
+import android.content.BroadcastReceiver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var citySearch: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        citySearch = findViewById(R.id.citySearch)
+
+        citySearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
+
+
+
 }
