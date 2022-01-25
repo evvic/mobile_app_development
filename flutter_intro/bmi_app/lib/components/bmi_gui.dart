14e9c1f80ae3c5d1fd978273d4e1a6b184d20395
Eric Brown <ewic98@gmail.com>
2022-01-25 16:31:15 +0700

    bmi ui elements

--- flutter_intro/bmi_app/lib/components/bmi_gui.dart
+++ flutter_intro/bmi_app/lib/components/bmi_gui.dart
@@ -3,11 +3,37 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BmiGui extends StatelessWidget {
+  final ButtonStyle style =
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
 
   @override
   Widget build(BuildContext context) {
-    return (const Center(
-      child: Text('Hello World'),
+    return (Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget> [
+          const Text("Weight"),
+          const TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Enter weight here",
+            ),
+          ),
+          const Text("Height"),
+          const TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Enter height here",
+            ),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('enabled'),
+
+          ),
+        ]
+      ,
     ));
   }
 }
