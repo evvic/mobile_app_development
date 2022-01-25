a46ad227778fe824addf602031727f1367f9384f
Eric Brown <ewic98@gmail.com>
2022-01-26 02:56:59 +0700

    list of people

--- person/bin/person.dart
+++ person/bin/person.dart
@@ -1,9 +1,21 @@
 import 'package:person/person.dart';
+import 'package:person/student.dart';
 
 void main(List<String> arguments) {
   //print('Hello world: ${person.calculate()}!');
   print("We in the console baby");
   Person edawg = Person.complete("Eric", 23, 180, 76);
 
-  print(edawg.toString());
+  //print(edawg.toString());
+
+  Person ewic = Student(123456, "EWIC", 23);
+  Person oscar = Student(928475, "Oscar", 24);
+
+  // growable list
+  var listy = [edawg, ewic];
+  listy.add(oscar);
+
+  for (var student in listy) {
+    print(student.toString());
+  }
 }
