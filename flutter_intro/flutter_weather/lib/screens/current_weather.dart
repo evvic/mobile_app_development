3c926e1a8644fab0419c1c29f2236c03624f6481
Eric Brown <ewic98@gmail.com>
2022-02-01 15:44:36 +0700

    changed current weather screen to stateful widget

--- flutter_intro/flutter_weather/lib/screens/current_weather.dart
+++ flutter_intro/flutter_weather/lib/screens/current_weather.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/screens/forecast.dart';
 
-class currentWeatherOnly extends StatelessWidget {
+class currentWeatherOnly extends StatefulWidget {
   const currentWeatherOnly({Key? key}) : super(key: key);
+
+  @override
+  State<currentWeatherOnly> createState() => _currentWeatherOnly();
+}
+
+class _currentWeatherOnly extends State<currentWeatherOnly> {
+  String cityName = "Bangkok";
+  String currentWeather = "Sunny";
+  double temp = 0;
+  double windSpeed = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bangkok'),
+        title: Text(cityName),
       ),
       body: Center(
-        child: Column(children: [
-          const Text("Sunny", style: TextStyle(fontSize: 40)),
-          const Text("-5C", style: TextStyle(fontSize: 40)),
-          const Text("3 m/s", style: TextStyle(fontSize: 40)),
-             ElevatedButton(
-              child: const Text('Get weather data'),
-              onPressed: () {
-                // Navigate to second route when tapped.
-                Navigator.push(
-                    context,
-                    // class we use to change to another page
-                    MaterialPageRoute(
-                        builder: (context) => const WeatherForecastScreen()));
-              },
-            ),
-        ],)
-      ),
+          child: Column(
+        // evenly space all children vertically on the one screen
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(currentWeather, style: TextStyle(fontSize: 40)),
+          Text("$temp C", style: TextStyle(fontSize: 40)),
+          Text("$windSpeed m/s", style: TextStyle(fontSize: 40)),
+          ElevatedButton(
+            child: const Text('Get weather data'),
+            onPressed: () {
+              // Navigate to second route when tapped.
+              Navigator.push(
+                  context,
+                  // class we use to change to another page
+                  MaterialPageRoute(
+                      builder: (context) => const WeatherForecastScreen()));
+            },
+          ),
+        ],
+      )),
     );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
