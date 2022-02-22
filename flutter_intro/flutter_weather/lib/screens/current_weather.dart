50a4c56500b102334b4780bb6f781c427393b9de
Eric Brown <ewic98@gmail.com>
2022-02-22 17:38:20 +0700

    use users location to get weather

--- flutter_intro/flutter_weather/lib/screens/current_weather.dart
+++ flutter_intro/flutter_weather/lib/screens/current_weather.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weather/screens/forecast.dart';
 import 'package:http/http.dart' as http;
 import 'package:sensors_plus/sensors_plus.dart';
+import 'package:location/location.dart';
 
 class CurrentWeatherOnly extends StatefulWidget {
   const CurrentWeatherOnly({Key? key}) : super(key: key);
@@ -18,14 +19,18 @@ class _CurrentWeatherOnly extends State<CurrentWeatherOnly> {
   double temp = 0;
   double windSpeed = 0;
   String icon = "01n";
+  // location
+  var location = new Location();
+  double latitude = 0.0;
+  double longitude = 0.0;
 
   void updateGUI() {}
 
-  void fetchWeather(String city) async {
-    city = "Sacramento";
-
+  void fetchWeather(double lat, double long) async {
     Uri url = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=6c433438776b5be4ac86001dc88de74d");
+        //"https://api.openweathermap.org/data/2.5/weather?q=$city&appid=6c433438776b5be4ac86001dc88de74d");
+        "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=6c433438776b5be4ac86001dc88de74d");
+
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -34,19 +39,49 @@ class _CurrentWeatherOnly extends State<CurrentWeatherOnly> {
       var weatherData = json.decode(response.body);
 
       setState(() {
-        cityName = city;
+        cityName = weatherData["name"];
         temp = weatherData['main']['temp'] - 273.15;
         currentWeather = weatherData['weather'][0]['description'];
         windSpeed = weatherData['wind']['speed'];
         icon = weatherData['weather'][0]['icon'];
+        latitude = lat;
+        longitude = long;
       });
 
       updateGUI(); // set state
     }
   }
 
-  void openWeatherForecast() {
-    
+  void openWeatherForecast() {}
+
+  Future<bool> getLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return false;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return false;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    fetchWeather(_locationData.latitude!, _locationData.longitude!);
+
+    return true;
   }
 
   @override
@@ -67,11 +102,12 @@ class _CurrentWeatherOnly extends State<CurrentWeatherOnly> {
           ),
           Text(temp.toStringAsFixed(2) + " C", style: TextStyle(fontSize: 40)),
           Text("$windSpeed m/s", style: TextStyle(fontSize: 40)),
+          Text(latitude.toString() + ', ' + longitude.toString()),
           ElevatedButton(
-            child: const Text('Get weather data'),
+            child: const Text('Get location'),
             onPressed: () {
               // fetch data from internet
-              fetchWeather('Sactown');
+              getLocation();
             },
           ),
           ElevatedButton(
